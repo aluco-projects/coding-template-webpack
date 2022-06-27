cdfe7224651295ab6aa66422f85543c8932b5914
kyohei-dan <kyohey.dan@gmail.com>
2022-06-27 22:51:58 +0900

    [Add] slickファイルを追加

--- parts/scripts.php
+++ parts/scripts.php
@@ -1,4 +1,4 @@
 <script src="/assets/js/jquery.min.js"></script>
-<!--script src="/assets/js/slick.min.js"></script-->
+<script src="/assets/js/slick.min.js"></script>
 <script src="/assets/js/utils.js"></script>
 <script src="/assets/js/script.js"></script>
